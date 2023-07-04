9225213bff1b2c9b53c8272f1e91aafb9a176c9a
Basil Keeler <basil.keeler@outlook.com>
2023-07-04 05:47:40 -0500

    fix swayidle

--- home/default.nix
+++ home/default.nix
@@ -33,7 +33,7 @@
         command = "${pkgs.swaylock}/bin/swaylock -f -c 000000";
       }
       {
-        timeout = 6;
+        timeout = 600;
         command = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
         resumeCommand = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
       }
